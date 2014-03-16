563c67af335176ea9a4ee41268e210a5cee987b7
Dhruv Maheshwari <dhwari@gmail.com>
2014-03-15 21:41:51 -0400

    fixed mongo db connection/local port

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ var express = require('express');
 var app = express();
 var Project = require('./models/projects.js').Project;
 var mongoose = require("mongoose");
-mongoose.connect( 'mongodb://localhost/express-projects' );
+
+var uristring =
+	process.env.MONGOLAB_URI ||
+	process.env.MONGOHQ_URL ||
+	'mongodb://localhost/express-projects';
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
 //controllers
 var controller = require('./controllers/controller.js');
 app.use(express.static(__dirname + '/public'));
@@ -14,7 +27,11 @@ app.use(express.bodyParser())
    .use(express.multipart());
 
 app.use(express.logger("default"));
-app.listen(3000);
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
 app.get("/", controller.index);
 app.get("/edit/:id", controller.edit);
