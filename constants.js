b4ed030794ee8e52683b1c459c9d263f501502a3
Aaron Michaels <aaronmichaels@gmail.com>
2017-03-03 01:31:01 -0500

    add constants file

---
+++ constants.js
@@ -0,0 +1,8 @@
+// This file holds application constants.  Most of these should be set as
+// environment variables in production.
+
+module.exports ={
+  "ACCESS_KEY_ID": "AKIAJ5CH34YLF3NAGJJA",
+  "SECRET_ACCESS_KEY": "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",
+  "MONGO_URI": "mongodb://localhost/express-projects"
+}
